d64014655913fe0f1def4e3b3e1ba9e17683c476
Dan Smith <daniel.smith8795@gmail.com>
2019-06-19 23:50:05 -0500

    Tracking keyup and selecting a word at random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,8 +16,33 @@ var guesses = 9;
 function start() {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
     ///choosing a random word from the list
-    console.log(selectedWord);
     lettersinword = selectedWord.split("");
+    numBlanks = lettersinword.length;
+    guesses = 9;
+    wrongGuesses = [];
+    blanksandrightguesses = [];
+
+    for (var i = 0; i < numBlanks; i++) {
+        blanksandrightguesses.push("_");
+
+    }
+    $("#letters").text(blanksandrightguesses.join(" "));
+    $("#guesses_left").text(guesses);
+    $("#wrong_guesses").text(wrongGuesses);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+
+
+
+    ///My console.log log
+    console.log(selectedWord);
     console.log(lettersinword);
+    console.log(numBlanks);
+    console.log(blanksandrightguesses);
 }
 start();
+
+$(document).keyup(function () {
+    var userinput = String.fromCharCode(event).toLowerCase();
+    console.log(userinput);
+});
