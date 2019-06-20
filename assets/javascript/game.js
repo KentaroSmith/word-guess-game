7edc26d76ae356351cafb7fa53144ccb16745a38
Dan Smith <daniel.smith8795@gmail.com>
2019-06-19 21:53:19 -0500

    Finally figured out that I linked my javascript file incorrectly, working on making sure my game can select a word at random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,23 @@
-var wordOptions = ["pikachu", "squirtel", "bulbasaur", "charmander"];
+/// Variables
+///-------------------------------------------------------
+var wordOptions = ["pikachu", "squirtel", "bulbasaur", "charmander", "pidgey"];
 var selectedWord = "";
 var lettersinword = [];
+var numBlanks = 0;
+var blanksandrightguesses = [];
+var wrongGuesses = [];
+
 
 var wins = 0;
 var losses = 0;
 var guesses = 9;
+/// Functions
+///-------------------------------------------------------
+function start() {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    ///choosing a random word from the list
+    console.log(selectedWord);
+    lettersinword = selectedWord.split("");
+    console.log(lettersinword);
+}
+start();
